1a1fc56999e0d7a1e05543eb1b2c476159a6ceec
practicante2syg <practicante2@syg.com.co>
2020-05-26 18:20:40 -0500

    uploaded files

---
+++ src/main/java/Gestion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    import java.io.*;
+import javax.swing.JFileChooser;
+    
+/**
+ *
+ * @author JUAN
+ */
+public class Gestion {
+
+    
+    
+    public Gestion(){
+        
+    }
+    
+    public void Leer(File archivo){
+ 
+    }
+    
+    public void leerTexto(){
+
+    }
+}
